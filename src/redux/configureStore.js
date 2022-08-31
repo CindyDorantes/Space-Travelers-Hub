dab22f696e8ed4d8ea32076b9888784425917886
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-08-31 12:15:48 -0500

    fetch data from the Dragons endpoint

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -2,9 +2,11 @@ import { configureStore } from '@reduxjs/toolkit';
 import { combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import missionReducer from './Mission';
+import dragonReducer from './dragons/dragons';
 
 const rootReducer = combineReducers({
   missions: missionReducer,
+  dragons: dragonReducer,
 });
 
 const store = configureStore({
