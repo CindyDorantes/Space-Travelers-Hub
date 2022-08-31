d7291a5bf8049a2e29358642e97d4ecbdcb2fecd
ijeoma <ijeomaodinko@gmail.com>
2022-08-31 11:23:48 -0400

    Add files

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
-import Profile from './components/Profile/Profile';
+import MyProfile from './components/My-Profile';
 // import Rockets from './components/Rockets/Rockets';
 // import Rockets from './components/Rockets/Rockets';
 import Rockets from './components/rockets/rockets';
@@ -16,7 +16,7 @@ function App() {
           <Route path="/" element={<Rockets />} />
           {/* <Route path="/missions" element={<Missions />} />
         <Route path="/dragons" element={<Dragons />} /> */}
-          <Route path="/profile" element={<Profile />} />
+          <Route path="/my-profile" element={<MyProfile />} />
         </Routes>
       </main>
     </div>
