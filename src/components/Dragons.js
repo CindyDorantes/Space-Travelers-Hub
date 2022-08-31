c9bcd1e9760104988efcde3a0e43a922c7e8ab55
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-08-31 12:17:26 -0500

    fix linter errors

--- src/components/Dragons.js
+++ src/components/Dragons.js
@@ -6,28 +6,12 @@ import { getDragons } from '../redux/dragons/dragons';
 const Dragons = () => {
   const dispatch = useDispatch();
   const dragons = useSelector((state) => state.dragons);
-  console.log(dragons);
 
   useEffect(() => {
     if (!dragons.length) {
       dispatch(getDragons());
     }
   }, []);
-
-  // const state = [
-  //   {
-  //     id: 'dragon1',
-  //     name: 'Dragon 1',
-  //     type: 'capsule',
-  //     flickr_images: 'https://farm9.staticflickr.com/8618/16649075267_d18cbb4342_b.jpg',
-  //   },
-  //   {
-  //     id: 'dragon2',
-  //     name: 'Dragon 2',
-  //     type: 'capsule',
-  //     flickr_images: 'https://farm8.staticflickr.com/7647/16581815487_6d56cb32e1_b.jpg',
-  //   },
-  // ];
 
   return (
     <div className="dragonContainer">
