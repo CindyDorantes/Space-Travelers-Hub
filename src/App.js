9639ebbaa0f1e890f8cffefdf35dcab5e5d5f76f
Cindy Dorantes <cindym.dorantes@gmail.com>
2022-08-31 10:21:25 -0500

    fix routes for dragons and myprofile pages

--- src/App.js
+++ src/App.js
@@ -4,12 +4,16 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar';
 import MissionList from './components/Missions/MissionList';
+import Dragons from './components/Dragons';
+import MyProfile from './components/My-Profile';
 
 const App = () => (
   <BrowserRouter>
     <Navbar />
     <Routes>
-      <Route path="Missions" element={<MissionList />} />
+      <Route path="/dragons" element={<Dragons />} />
+      <Route path="/missions" element={<MissionList />} />
+      <Route path="/my-profile" element={<MyProfile />} />
     </Routes>
   </BrowserRouter>
 );
