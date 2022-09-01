91bf4362cd33f0a80870efd811a5a08a6071417a
ijeoma <ijeomaodinko@gmail.com>
2022-09-01 18:47:48 -0400

    Add reserved rockets in Myprofile

--- src/components/My-Profile.js
+++ src/components/My-Profile.js
@@ -1,26 +1,5 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 
-const MyProfile = () => {
-  const rockets = useSelector((state) => state.Rockets);
-  const reserveRocket = rockets.filter((rocket) => rocket.reserved);
-
-  return (
-    <div className="rocketDiv">
-      <h1>Rockets</h1>
-      <div>
-        {reserveRocket.length ? (
-          <div>
-            {reserveRocket.map((rocket) => (
-              <h2 className="rocketDesc" key={rocket.id}>
-                {rocket.name}
-              </h2>
-            ))}
-          </div>
-        ) : null}
-      </div>
-    </div>
-  );
-};
 
 export default MyProfile;
