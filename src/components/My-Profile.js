cb26a1582d788d216d95171447d08aecae6d7e35
Eternalgratis <tyaps002@gmail.com>
2022-09-02 00:27:32 +0100

    Display join mission in profile

--- src/components/My-Profile.js
+++ src/components/My-Profile.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
 
-const MyProfile = () => (
-  <div>
-    this is my profile
-  </div>
-);
+const MyProfile = () => {
+    const missionProfile = useSelector((state) => [
+      {
+        MissionTitle: 'My Mission',
+        data: state.missions.filter((mission) => mission.reserved),
+      },
+      ]);
+
+      return (
+      <div>
+          <div>
+            {missionProfile.map(({ MissionTitle, data }) => (
+              <div>
+                <h4>{MissionTitle}</h4>
+                <div className='content'>
+                  {
+                    !data.length
+                      ? <p>No Missions</p>
+                      : data.map((item) => <p key={item.id}>{item.name}</p>)
+                  }
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )
+  };
 
 export default MyProfile;
