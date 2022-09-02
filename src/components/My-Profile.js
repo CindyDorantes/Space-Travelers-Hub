9060bb22533b5f30e6f68145e3f8a80606ab6b62
Eternalgratis <tyaps002@gmail.com>
2022-09-02 17:31:07 +0100

    Fix linter error

--- src/components/My-Profile.js
+++ src/components/My-Profile.js
@@ -2,31 +2,31 @@ import React from 'react';
 import { useSelector } from 'react-redux';
 
 const MyProfile = () => {
-    const missionProfile = useSelector((state) => [
-      {
-        MissionTitle: 'My Mission',
-        data: state.missions.filter((mission) => mission.reserved),
-      },
-      ]);
+  const missionProfile = useSelector((state) => [
+    {
+      MissionTitle: 'My Mission',
+      data: state.missions.filter((mission) => mission.reserved),
+    },
+  ]);
 
-      return (
+  return (
+    <div>
       <div>
-          <div>
-            {missionProfile.map(({ MissionTitle, data, id }) => (
-              <div key={id}>
-                <h4 key={id}>{MissionTitle}</h4>
-                <div className="content">
-                  {
+        {missionProfile.map(({ MissionTitle, data, id }) => (
+          <div key={id}>
+            <h4 key={id}>{MissionTitle}</h4>
+            <div className="content">
+              {
                     !data.length
                       ? <p>No Missions</p>
                       : data.map((item) => <p key={item.id}>{item.name}</p>)
                   }
-                </div>
-              </div>
-            ))}
+            </div>
           </div>
-        </div>
-      )
-  };
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default MyProfile;
