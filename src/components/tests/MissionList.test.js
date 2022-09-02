254fd2d263dd3a188c09d040a7a0509690bcb30e
Eternalgratis <tyaps002@gmail.com>
2022-09-02 21:48:06 +0100

    Fix linters

--- src/components/tests/MissionList.test.js
+++ src/components/tests/MissionList.test.js
@@ -1,7 +1,7 @@
 import { Provider } from 'react-redux';
 import renderer from 'react-test-renderer';
 import MissionList from '../Missions/MissionList';
-import store from '../../redux/configureStore'
+import store from '../../redux/configureStore';
 
 describe('MyMissionList components', () => {
   it('MissionList Components Renders correctly', () => {
