071ddc15d1ebd73b11dc7b32119f557e0f0e2835
ijeoma <ijeomaodinko@gmail.com>
2022-09-02 16:50:39 -0400

    fix linters

--- src/components/tests/Dragons.test.js
+++ src/components/tests/Dragons.test.js
@@ -1,17 +1,17 @@
-import renderer from 'react-test-renderer'
+import renderer from 'react-test-renderer';
 import { Provider } from 'react-redux';
 import React from 'react';
 import store from '../../redux/configureStore';
 import Dragons from '../Dragons';
 
 describe('Dragons component', () => {
-it('Renders Dragons component', () => {
+  it('Renders Dragons component', () => {
     const tree = renderer
-    .create(
+      .create(
         <Provider store={store}>
-            <Dragons />
+          <Dragons />
         </Provider>,
-    );
+      );
     expect(tree).toMatchSnapshot();
   });
 });
--- src/components/tests/My-profile.test.js
+++ src/components/tests/My-profile.test.js
@@ -1,17 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
+import renderer from 'react-test-renderer';
 import store from '../../redux/configureStore';
-import renderer  from 'react-test-renderer';
 import MyProfile from '../My-Profile';
 
-
 describe('to test the rockets elements', () => {
-    it('matches DOM rockets snapshot', () => {
-      const tree  = renderer.create(
-          <Provider store={store}>
-            <MyProfile />
-          </Provider>,
-      );
-        expect(tree).toMatchSnapshot();
-    });
-})
+  it('matches DOM rockets snapshot', () => {
+    const tree = renderer.create(
+      <Provider store={store}>
+        <MyProfile />
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+});
